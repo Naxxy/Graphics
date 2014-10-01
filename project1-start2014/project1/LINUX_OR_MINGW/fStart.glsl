8dcd97c38b2e73a4ae4a924dafbdf86a35442d86
Naxxy <20933584@student.uwa.edu.au>
2014-10-01 16:11:31 +0800

    Completed part G: - Moved the main lighting calculations into the fragment shader. - Deleted main lighting calculations from the vector shader. - Added pass-through code where necessary for passing values from vertex shader to fragment shader.

--- project1-start2014/project1/LINUX_OR_MINGW/fStart.glsl
+++ project1-start2014/project1/LINUX_OR_MINGW/fStart.glsl
@@ -1,7 +1,13 @@
 #version 150
 
-in  vec4 color;
 in  vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+in  vec3 fPos;
+in  vec3 fNormal;
+
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform mat4 ModelView;
+uniform vec4 LightPosition;
+uniform float Shininess;
 
 out vec4 fColor;
 
@@ -10,5 +16,36 @@ uniform sampler2D texture;
 void
 main()
 {
-    fColor = color * texture2D( texture, texCoord * 2.0 );
+    
+    // The vector to the light from the vertex    
+    vec3 Lvec = LightPosition.xyz - fPos;
+    float Ldist = length(Lvec);
+
+    // Unit direction vectors for Blinn-Phong shading calculation
+    vec3 L = normalize( Lvec );   // Direction to the light source
+    vec3 E = normalize( -fPos );   // Direction to the eye/camera
+    vec3 H = normalize( L + E );  // Halfway vector
+
+    // Transform vertex normal into eye coordinates (assumes scaling is uniform across dimensions)
+    vec3 N = normalize( (ModelView*vec4(fNormal, 0.0)).xyz );
+
+    // Compute terms in the illumination equation
+    vec3 ambient = AmbientProduct/pow(Ldist, 0.75);
+
+    float Kd = max( dot(L, N), 0.0 );
+    vec3  diffuse = Kd*DiffuseProduct/pow(Ldist, 0.75);
+
+    float Ks = pow( max(dot(N, H), 0.0), Shininess );
+    vec3  specular = Ks * SpecularProduct/pow(Ldist, 0.75);
+    
+    if( dot(L, N) < 0.0 ) {
+	specular = vec3(0.0, 0.0, 0.0);
+    } 
+
+    // globalAmbient is independent of distance from the light source
+    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+    fColor.rgb = globalAmbient  + ambient + diffuse + specular;
+    fColor.a = 1.0;
+    
+    fColor = fColor * texture2D( texture, texCoord * 2.0 );
 }
--- project1-start2014/project1/LINUX_OR_MINGW/vStart.glsl
+++ project1-start2014/project1/LINUX_OR_MINGW/vStart.glsl
@@ -5,51 +5,20 @@ in  vec3 vNormal;
 in  vec2 vTexCoord;
 
 out vec2 texCoord;
-out vec4 color;
+out vec3 fPos;
+out vec3 fNormal;
 
-uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform mat4 ModelView;
-uniform mat4 Projection;
-uniform vec4 LightPosition;
-uniform float Shininess;
-
 
 void main()
 {
     // Transform vertex position into eye coordinates
     vec3 pos = (ModelView * vPosition).xyz;
-
-
-    // The vector to the light from the vertex    
-    vec3 Lvec = LightPosition.xyz - pos;
-    float Ldist = length(Lvec);//pow(Lvec.x*Lvec.x + Lvec.y*Lvec.y + Lvec.z*Lvec.z, 0.5);	// Added
-
-    // Unit direction vectors for Blinn-Phong shading calculation
-    vec3 L = normalize( Lvec );   // Direction to the light source
-    vec3 E = normalize( -pos );   // Direction to the eye/camera
-    vec3 H = normalize( L + E );  // Halfway vector
-
-    // Transform vertex normal into eye coordinates (assumes scaling is uniform across dimensions)
-    vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
-
-    // Compute terms in the illumination equation
-    vec3 ambient = AmbientProduct/pow(Ldist, 0.75);
-
-    float Kd = max( dot(L, N), 0.0 );
-    vec3  diffuse = Kd*DiffuseProduct/pow(Ldist, 0.75);
-
-    float Ks = pow( max(dot(N, H), 0.0), Shininess );
-    vec3  specular = Ks * SpecularProduct/pow(Ldist, 0.75);
     
-    if( dot(L, N) < 0.0 ) {
-	specular = vec3(0.0, 0.0, 0.0);
-    } 
-
-    // globalAmbient is independent of distance from the light source
-    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
-    color.rgb = globalAmbient  + ambient + diffuse + specular;
-    color.a = 1.0;
-
-    gl_Position = Projection * ModelView * vPosition;				// not this
-    texCoord = vTexCoord;							// not this
+    gl_Position = Projection * ModelView * vPosition;
+    texCoord = vTexCoord;
+    
+    // Pass values through for calculation in fragment shader
+    fPos = pos;
+    fNormal = vNormal;
 }
